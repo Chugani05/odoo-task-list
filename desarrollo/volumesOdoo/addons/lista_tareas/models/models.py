3e2cdd62f0b7e3f297c140e004d30051cd2629b7
chugani05 <chugani205@gmail.com>
2025-12-11 16:12:48 +0000

    add fecha-limite, fecha-creacion and retrasada model and view

--- desarrollo/volumesOdoo/addons/lista_tareas/models/models.py
+++ desarrollo/volumesOdoo/addons/lista_tareas/models/models.py
@@ -10,13 +10,24 @@ class ListaTareas(models.Model):
     prioridad = fields.Integer(string='Prioridad')
     urgente = fields.Boolean(
         string='Urgente',
-        compute='_value_urgente',
+        compute='_check_urgency',
         store=True
     )
     realizada = fields.Boolean(string='Realizada')
     asignado_a = fields.Many2one('res.users', string='Asignado a', required=False, default=lambda self: self.env.user)
+    fecha_limite = fields.Date(string='Fecha limite', required=False)
+    fecha_creacion = fields.Datetime(string='Fecha de creacion', default=lambda self: fields.Datetime.today())
+    retrasada = fields.Boolean(string='retrasada', compute='_check_delayed', store=True)
 
     @api.depends('prioridad')
-    def _value_urgente(self):
+    def _check_urgency(self):
         for record in self:
             record.urgente = record.prioridad > 10
+
+    @api.depends('fecha_limite', 'retrasada')
+    def _check_delayed(self):
+        for record in self:
+            if record.fecha_limite: 
+                record.realizada = False and record.fecha_limite < fields.Date.today()
+            else:
+                record.retrasada = False
