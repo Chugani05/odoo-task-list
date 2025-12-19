ac13352680ea380db74641d25588ffd0558cf9eb
Chugani05 <rashi.chugani.n@gmail.com>
2025-12-19 00:01:43 +0000

    add buttons to form; add kanban; add images

--- desarrollo/volumesOdoo/addons/lista_tareas/models/models.py
+++ desarrollo/volumesOdoo/addons/lista_tareas/models/models.py
@@ -13,11 +13,22 @@ class ListaTareas(models.Model):
         compute='_check_urgency',
         store=True
     )
-    realizada = fields.Boolean(string='Realizada')
+    realizada = fields.Boolean(string='Realizada', compute='_check_done', store=True)
     asignado_a = fields.Many2one('res.users', string='Asignado a', required=False, default=lambda self: self.env.user)
     fecha_limite = fields.Date(string='Fecha limite', required=False)
     fecha_creacion = fields.Datetime(string='Fecha de creacion', default=lambda self: fields.Datetime.now())
     retrasada = fields.Boolean(string='Retrasada', compute='_check_delayed', store=True)
+    estado = fields.Selection([
+                                ('nuevo', 'Nueva'), 
+                                ('progreso', 'En curso'), 
+                                ('bloqueado', 'Bloqueada'), 
+                                ('hecho', 'Hecha')
+                            ], 
+                              string="Estado", 
+                              default="nuevo", 
+                              compute='_check_state', 
+                              store=True
+                            )
 
     @api.depends('prioridad')
     def _check_urgency(self):
@@ -31,3 +42,23 @@ class ListaTareas(models.Model):
                 record.retrasada = not record.realizada and record.fecha_limite < fields.Date.today()
             else:
                 record.retrasada = False
+
+    @api.depends('realizada')
+    def _check_state(self):
+        for record in self:
+            if record.realizada:
+                record.estado = "hecho"
+
+    @api.depends('estado')
+    def _check_done(self):
+        for record in self:
+            record.realizada = record.estado == "hecho"
+
+    def set_state_to_in_progress(self):
+        self.write({'state': "progreso"})
+
+    def set_state_to_blocked(self):
+        self.write({'state': "bloqueado"})
+
+    def set_state_to_done(self):
+        self.write({'state': "hecho"})
